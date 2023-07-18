c544fee907c372fb120959b868f5dfc8c92c79d4
igoogolx <27353191+igoogolx@users.noreply.github.com>
2023-07-18 10:33:38 -0700

    chore(build): remove splitChunks for ui building config

--- build/webpack.ui.config.js
+++ build/webpack.ui.config.js
@@ -13,27 +13,6 @@ module.exports = (env) => {
     optimization: {
       minimize: !isDev,
       usedExports: true,
-      splitChunks: {
-        chunks: "async",
-        minSize: 20000,
-        minRemainingSize: 0,
-        minChunks: 1,
-        maxAsyncRequests: 30,
-        maxInitialRequests: 30,
-        enforceSizeThreshold: 50000,
-        cacheGroups: {
-          defaultVendors: {
-            test: /[\\/]node_modules[\\/]/,
-            priority: -10,
-            reuseExistingChunk: true,
-          },
-          default: {
-            minChunks: 2,
-            priority: -20,
-            reuseExistingChunk: true,
-          },
-        },
-      },
     },
     devtool: isDev ? "source-map" : false,
     entry: path.resolve(
