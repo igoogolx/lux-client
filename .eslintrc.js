f26206e638102a4006099035f258593e86de33ec
igoogolx <igoogolx@gmail.com>
2023-09-02 13:27:55 +0800

    chore: fix eslint resolve errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,29 @@
 module.exports = {
-  root:true,
+  root: true,
   extends: ["airbnb-typescript-prettier"],
+  plugins: ["import"],
   rules: {
     "import/prefer-default-export": "off",
-    "import/no-extraneous-dependencies":"off",
+    "import/no-extraneous-dependencies": "off",
+    "import/no-unresolved": "error",
     "prettier/prettier": [
       "error",
       {
-        endOfLine: "auto",
-      },
-    ],
+        endOfLine: "auto"
+      }
+    ]
   },
+  settings: {
+    "import/resolver": {
+      "typescript": {
+        "alwaysTryTypes": true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+
+        // Choose from one of the "project" configs below or omit to use <root>/tsconfig.json by default
+
+        // use <root>/path/to/folder/tsconfig.json
+        "project": "./tsconfig.json",
+
+      }
+    }
+  }
 };
