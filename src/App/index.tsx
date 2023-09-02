6dfc548dcffc89cf3c76c6fd5ae8377e52c95a9f
igoogolx <27353191+igoogolx@users.noreply.github.com>
2023-09-02 10:51:34 -0700

    feat: only show modal when disconnected

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -85,42 +85,50 @@ export function App(): React.ReactNode {
     };
   }, [dispatch]);
   return (
-    <div className={styles.wrapper} id={APP_CONTAINER_ID}>
-      <NotificationContainer />
-      <ServerConfirmModal />
-      {!connected && <CheckHubAddressModal />}
-      <ElevateModal />
-      <div className={styles.body}>
-        <div
-          className={classNames(
-            { [styles.expandedNav]: isNavOpen },
-            styles.nav,
-            isNavOpen ? inlineStyles.expandedNav : inlineStyles.nav
-          )}
-        >
-          <Nav
-            onClick={() => {
-              if (isNavOpen) {
-                setIsNavOpen(false);
-              }
-            }}
-          />
-          <div className={styles.themeSwitch}>
-            <ThemeSwitch />
+    <>
+      {!connected ? (
+        <CheckHubAddressModal />
+      ) : (
+        <div className={styles.wrapper} id={APP_CONTAINER_ID}>
+          <NotificationContainer />
+          <ServerConfirmModal />
+          <ElevateModal />
+          <div className={styles.body}>
+            <div
+              className={classNames(
+                { [styles.expandedNav]: isNavOpen },
+                styles.nav,
+                isNavOpen ? inlineStyles.expandedNav : inlineStyles.nav
+              )}
+            >
+              <Nav
+                onClick={() => {
+                  if (isNavOpen) {
+                    setIsNavOpen(false);
+                  }
+                }}
+              />
+              <div className={styles.themeSwitch}>
+                <ThemeSwitch />
+              </div>
+            </div>
+            <div className={styles.content}>
+              <Header isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
+              <Routes>
+                <Route path={ROUTER_PATH.Home} element={<Home />} />
+                <Route path={ROUTER_PATH.Dashboard} element={<Dashboard />} />
+                <Route
+                  path={ROUTER_PATH.Connections}
+                  element={<Connections />}
+                />
+                <Route path={ROUTER_PATH.Logger} element={<Logger />} />
+                <Route path={ROUTER_PATH.Setting} element={<Setting />} />
+                <Route path={ROUTER_PATH.About} element={<About />} />
+              </Routes>
+            </div>
           </div>
         </div>
-        <div className={styles.content}>
-          <Header isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
-          <Routes>
-            <Route path={ROUTER_PATH.Home} element={<Home />} />
-            <Route path={ROUTER_PATH.Dashboard} element={<Dashboard />} />
-            <Route path={ROUTER_PATH.Connections} element={<Connections />} />
-            <Route path={ROUTER_PATH.Logger} element={<Logger />} />
-            <Route path={ROUTER_PATH.Setting} element={<Setting />} />
-            <Route path={ROUTER_PATH.About} element={<About />} />
-          </Routes>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
