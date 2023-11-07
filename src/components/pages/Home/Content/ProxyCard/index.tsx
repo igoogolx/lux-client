a021cb26905aa22da6b4216f7aff72ac8b034753
igoogolx <igoogolx@gmail.com>
2023-11-07 10:59:14 +0800

    feat(proxy-card): collapsible

--- src/components/pages/Home/Content/ProxyCard/index.tsx
+++ src/components/pages/Home/Content/ProxyCard/index.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { notifier, Table } from "@/components/Core";
 import {
+  Accordion,
+  AccordionHeader,
+  AccordionItem,
+  AccordionPanel,
   Badge,
   Button,
   Card,
@@ -84,68 +88,76 @@ export default function ProxyCard<T extends { id: string }>(
 
   return (
     <Card className={styles.card}>
-      <CardHeader
-        header={
-          <Badge appearance="outline" size="large">
-            {url === LOCAL_SERVERS
-              ? t(TRANSLATION_KEY.LOCAL_SERVERS)
-              : new URL(url).hostname}
-          </Badge>
-        }
-        className={styles.header}
-        action={
-          <div className={styles.action}>
-            <Tooltip
-              content={t(TRANSLATION_KEY.COMMON_DELETE)}
-              relationship="description"
-            >
-              <Button
-                onClick={handleDeleteProxies}
-                icon={<DeleteRegular />}
-                className={mergeClasses(
-                  styles.btn,
-                  isStarted ? "" : inlineStyles.danger
+      <Accordion collapsible defaultOpenItems={["1"]}>
+        <AccordionItem value="1">
+          <CardHeader
+            header={
+              <AccordionHeader>
+                <Badge appearance="outline" size="large">
+                  {url === LOCAL_SERVERS
+                    ? t(TRANSLATION_KEY.LOCAL_SERVERS)
+                    : new URL(url).hostname}
+                </Badge>
+              </AccordionHeader>
+            }
+            className={styles.header}
+            action={
+              <div className={styles.action}>
+                <Tooltip
+                  content={t(TRANSLATION_KEY.COMMON_DELETE)}
+                  relationship="description"
+                >
+                  <Button
+                    onClick={handleDeleteProxies}
+                    icon={<DeleteRegular />}
+                    className={mergeClasses(
+                      styles.btn,
+                      isStarted ? "" : inlineStyles.danger
+                    )}
+                    disabled={isStarted}
+                  />
+                </Tooltip>
+                {url !== LOCAL_SERVERS && (
+                  <Tooltip
+                    content={t(TRANSLATION_KEY.COMMON_COPY_URL)}
+                    relationship="description"
+                  >
+                    <Button
+                      onClick={handleCopyUrl}
+                      icon={<ClipboardRegular />}
+                      className={styles.btn}
+                      disabled={isStarted}
+                    />
+                  </Tooltip>
                 )}
-                disabled={isStarted}
-              />
-            </Tooltip>
-            {url !== LOCAL_SERVERS && (
-              <Tooltip
-                content={t(TRANSLATION_KEY.COMMON_COPY_URL)}
-                relationship="description"
-              >
-                <Button
-                  onClick={handleCopyUrl}
-                  icon={<ClipboardRegular />}
-                  className={styles.btn}
-                  disabled={isStarted}
-                />
-              </Tooltip>
-            )}
-            {url !== LOCAL_SERVERS && (
-              <Tooltip
-                content={t(TRANSLATION_KEY.UPDATE_CLASH_PROXIES)}
-                relationship="description"
-              >
-                <Button
-                  onClick={handleUpdateClashProxies}
-                  icon={<ArrowSyncRegular />}
-                  className={styles.btn}
-                  disabled={isStarted}
-                />
-              </Tooltip>
-            )}
-          </div>
-        }
-      />
-      <Table
-        columns={columns}
-        data={data}
-        selectionMode={selectionMode}
-        onSelectionChange={onSelectionChange}
-        selectedItems={selectedItems}
-        getRowId={(item) => item.id}
-      />
+                {url !== LOCAL_SERVERS && (
+                  <Tooltip
+                    content={t(TRANSLATION_KEY.UPDATE_CLASH_PROXIES)}
+                    relationship="description"
+                  >
+                    <Button
+                      onClick={handleUpdateClashProxies}
+                      icon={<ArrowSyncRegular />}
+                      className={styles.btn}
+                      disabled={isStarted}
+                    />
+                  </Tooltip>
+                )}
+              </div>
+            }
+          />
+          <AccordionPanel>
+            <Table
+              columns={columns}
+              data={data}
+              selectionMode={selectionMode}
+              onSelectionChange={onSelectionChange}
+              selectedItems={selectedItems}
+              getRowId={(item) => item.id}
+            />
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
     </Card>
   );
 }
