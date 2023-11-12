63daca8bf583bb48b4143d7fd1624ad44849145e
igoogolx <27353191+igoogolx@users.noreply.github.com>
2023-11-11 22:20:00 -0800

    fix(setting): unstable loading

--- src/components/pages/Setting/index.tsx
+++ src/components/pages/Setting/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import { getSetting } from "lux-js-sdk";
 import { useDispatch } from "react-redux";
 import { generalSlice, settingSlice } from "@/reducers";
@@ -7,16 +7,27 @@ import styles from "./index.module.css";
 
 export default function Setting() {
   const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(generalSlice.actions.setLoading({ loading: true }));
-    getSetting()
-      .then((data) => {
-        dispatch(settingSlice.actions.setSetting(data));
-      })
-      .finally(() => {
+  const timer = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const init = useCallback(async () => {
+    try {
+      timer.current = setTimeout(() => {
+        dispatch(generalSlice.actions.setLoading({ loading: true }));
+        timer.current = null;
+      }, 200);
+      const data = await getSetting();
+      dispatch(settingSlice.actions.setSetting(data));
+    } finally {
+      if (timer.current) {
+        clearTimeout(timer.current);
+      } else {
         dispatch(generalSlice.actions.setLoading({ loading: false }));
-      });
+      }
+    }
   }, [dispatch]);
+
+  useEffect(() => {
+    init();
+  }, [init]);
 
   return (
     <div className={styles.wrapper}>
