bc1c9716f4f0fb2f6d46c3854ffb5cc4a0a926dc
igoogolx <27353191+igoogolx@users.noreply.github.com>
2024-12-21 16:35:17 +0800

    feat: fix eslint

--- src/components/Core/Menu/index.tsx
+++ src/components/Core/Menu/index.tsx
@@ -1,16 +1,14 @@
-import type * as React from 'react'
-import { type MenuItemSlots } from '@fluentui/react-components'
-import { type IconProps } from '../Icon'
+import type * as React from "react";
+import { type MenuItemSlots } from "@fluentui/react-components";
+import { type IconProps } from "../Icon";
 
 export interface MenuItemProps {
-  id: string | number
-  // TODO: remove
-  // eslint-disable-next-line react/no-unused-prop-types
-  icon?: MenuItemSlots['icon']
-  content?: React.ReactNode
-  iconName?: IconProps['name']
-  isDivider?: boolean
-  isDanger?: boolean
-  style?: React.CSSProperties
-  disabled?: boolean
+  id: string | number;
+  icon?: MenuItemSlots["icon"];
+  content?: React.ReactNode;
+  iconName?: IconProps["name"];
+  isDivider?: boolean;
+  isDanger?: boolean;
+  style?: React.CSSProperties;
+  disabled?: boolean;
 }
