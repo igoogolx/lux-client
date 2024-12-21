44efda6552ba92bc99b9521b1b2c545e8beb1154
igoogolx <27353191+igoogolx@users.noreply.github.com>
2024-12-21 16:25:43 +0800

    chore: fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,17 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "path";
+import { fileURLToPath } from "url";
 
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname
+});
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -13,6 +23,6 @@ export default [
   pluginReact.configs.flat.recommended,
   {
     ignores:['dist','build'],
-    extends: ['"plugin:react-hooks/recommended"']
   },
+  ...compat.extends('plugin:react-hooks/recommended'),
 ];
