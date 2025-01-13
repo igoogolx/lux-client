0831f70ff92175435ccd9128a3a79c5061d1ee6e
igoogolx <27353191+igoogolx@users.noreply.github.com>
2025-01-13 19:41:46 +0800

    refactor(Splash): implement with Spinner

--- src/components/Splash/index.tsx
+++ src/components/Splash/index.tsx
@@ -1,12 +1,12 @@
+import { Spinner } from "@fluentui/react-components";
 import React from "react";
-import { Icon, IconNameEnum } from "../Core";
 import styles from "./index.module.css";
 
 export default function Splash(): React.ReactNode {
   return (
     <div className={styles.container}>
       <div className={styles.overlay} />
-      <Icon name={IconNameEnum.Spin} className={styles.icon} />
+      <Spinner />
     </div>
   );
 }
