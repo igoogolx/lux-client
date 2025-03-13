dcdb0349505b88f1e1b89daa949c7b0fed6c45e8
igoogolx <27353191+igoogolx@users.noreply.github.com>
2025-03-13 19:29:32 +0800

    fix: always enable AutoLaunch component in Form

--- src/components/pages/Setting/Form.tsx
+++ src/components/pages/Setting/Form.tsx
@@ -36,7 +36,7 @@ export function SettingForm() {
       <div>
         <Language />
         <Theme />
-        {isDarwin && <AutoLaunch />}
+        <AutoLaunch />
         <AutoConnect />
         <Mode />
         {isTun && <Stack />}
