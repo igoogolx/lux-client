cd521dab8261bd6947c987474489ae42c049c7c3
igoogolx <27353191+igoogolx@users.noreply.github.com>
2025-04-19 11:52:52 +0800

    refactor: remove unused code

--- src/components/pages/Setting/index.tsx
+++ src/components/pages/Setting/index.tsx
@@ -1,37 +1,19 @@
-import { generalSlice, settingSlice } from "@/reducers";
+import { settingSlice } from "@/reducers";
 import { getSetting } from "lux-js-sdk";
-import React, { useCallback, useEffect, useRef } from "react";
+import React, { useCallback, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { SettingForm } from "./Form";
 import styles from "./index.module.css";
 
 export default function Setting() {
   const dispatch = useDispatch();
-  const timer = useRef<ReturnType<typeof setTimeout> | null>(null);
   const init = useCallback(async () => {
-    try {
-      timer.current = setTimeout(() => {
-        dispatch(generalSlice.actions.setLoading({ loading: true }));
-        timer.current = null;
-      }, 200);
-      const data = await getSetting();
-      dispatch(settingSlice.actions.setSetting(data));
-    } finally {
-      if (timer.current) {
-        clearTimeout(timer.current);
-      } else {
-        dispatch(generalSlice.actions.setLoading({ loading: false }));
-      }
-    }
+    const data = await getSetting();
+    dispatch(settingSlice.actions.setSetting(data));
   }, [dispatch]);
 
   useEffect(() => {
     init();
-    return () => {
-      if (timer.current) {
-        clearTimeout(timer.current);
-      }
-    };
   }, [init]);
 
   return (
