a39e61cbe62643bba07bddfbb776e0b7ffa994e4
igoogolx <27353191+igoogolx@users.noreply.github.com>
2025-07-07 07:45:10 -0700

    feat(Setting): support fake ip

--- src/i18n/locales/zh_cn.ts
+++ src/i18n/locales/zh_cn.ts
@@ -215,8 +215,6 @@ export default {
   [TRANSLATION_KEY.DIAGNOSTICS]: "诊断",
   [TRANSLATION_KEY.REQUIRED]: "必填",
   [TRANSLATION_KEY.MIXED_MODE]: "混合",
-  [TRANSLATION_KEY.FAKE_IP_SWITCH_LABEL]: "Fake Ip",
-  //TODO: update
-  [TRANSLATION_KEY.FAKE_IP_SWITCH_TOOLTIP]:
-    "Once enabled, remote dns will be forwarded to proxy",
+  [TRANSLATION_KEY.FAKE_IP_SWITCH_LABEL]: "Fake Ip (实验性)",
+  [TRANSLATION_KEY.FAKE_IP_SWITCH_TOOLTIP]: "开启以后，远程 DNS 会被转发给代理",
 };
