18abfd40e365aeb54a4fca57a85533078436a8c8
igoogolx <27353191+igoogolx@users.noreply.github.com>
2025-11-01 13:41:11 +0800

    feat: upgrade tailwindcss@v4

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,7 @@ import { StrictMode, useMemo, useState } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { HashRouter as Router } from "react-router-dom";
+import "tailwindcss/preflight.css";
 import { App } from "./App";
 import "./index.css";
 import { store } from "./reducers";
