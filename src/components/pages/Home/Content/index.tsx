3faf3687e1ad719917621b1ff684a1eff1000430
igoogolx <27353191+igoogolx@users.noreply.github.com>
2025-11-10 00:44:35 +0800

    v1.19.1 (#75)
    * feat(Home): change icon of proxy item menu to MoreHorizontalFilled
    
    * chore: update eslint rules of React
    
    * feat(Home): open proxy editing modal from url
    
    * feat(Setting): update i18n
    
    * refactor(Home): remove unused code
    
    * chore: v1.19.1

--- src/components/pages/Home/Content/index.tsx
+++ src/components/pages/Home/Content/index.tsx
@@ -1,10 +1,13 @@
 import SensitiveInfo from "@/components/Core/SensitiveInfo";
+import { EditModal } from "@/components/Modal/Proxy";
+import { QrCodeModal } from "@/components/Modal/QrCodeModal";
 import {
   proxiesSelectors,
   proxiesSlice,
   type RootState,
   selectedSlice,
 } from "@/reducers";
+import { encode } from "@/utils/url";
 import {
   createTableColumn,
   type DataGridProps,
@@ -13,20 +16,24 @@ import {
 } from "@fluentui/react-components";
 import {
   type BaseProxy,
-  getProxies, ProxyTypeEnum,
-  type SettingRes, type Shadowsocks,
+  getProxies,
+  ProxyTypeEnum,
+  type SettingRes,
+  type Shadowsocks,
   updateSelectedProxyId,
 } from "lux-js-sdk";
 import * as React from "react";
-import {useEffect, useMemo, useRef, useState} from "react";
+import { useEffect, useEffectEvent, useMemo, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useSearchParams } from "react-router-dom";
 import ProxyCard, { LOCAL_SERVERS } from "./ProxyCard";
 import { DelayTag } from "./ProxyCard/DelayTag";
 import { Operation } from "./ProxyCard/Operation";
 import styles from "./index.module.css";
-import {QrCodeModal} from "@/components/Modal/QrCodeModal";
-import {encode} from "@/utils/url";
-import {EditModal} from "@/components/Modal/Proxy";
+
+enum ROUTE_PARAM_MODE {
+  EDIT = "edit",
+}
 
 export function Content(): React.ReactNode {
   const proxies = useSelector(proxiesSelectors.selectAll);
@@ -49,23 +56,43 @@ export function Content(): React.ReactNode {
   const selectedId = useSelector<RootState, string>(
     (state) => state.selected.proxy,
   );
+  const [isEditingDialogOpen, setIsEditingDialogOpen] = useState(false);
+  const [operatingProxy, setOperatingProxy] = useState<BaseProxy | null>(null);
+
   const dispatch = useDispatch();
-  useEffect(() => {
-    getProxies().then((data) => {
-      dispatch(proxiesSlice.actions.received(data));
-      dispatch(selectedSlice.actions.setProxy({ id: data.selectedId }));
-    });
-  }, [dispatch]);
+
+  const [searchParams] = useSearchParams();
 
   const onEdit = (proxy: BaseProxy) => {
     setOperatingProxy(proxy);
     setIsEditingDialogOpen(true);
-  }
+  };
+
+  const handleEditFromUrl = useEffectEvent((proxies: BaseProxy[]) => {
+    const mode = searchParams.get("mode");
+    const proxyId = searchParams.get("proxyId");
+    if (mode !== ROUTE_PARAM_MODE.EDIT || typeof proxyId !== "string") {
+      return;
+    }
+    const targetProxy = proxies.find((proxy) => proxy.id === proxyId);
+    if (!targetProxy) {
+      return;
+    }
+    onEdit(targetProxy);
+  });
+
+  useEffect(() => {
+    getProxies().then((data) => {
+      dispatch(proxiesSlice.actions.received(data));
+      dispatch(selectedSlice.actions.setProxy({ id: data.selectedId }));
+      handleEditFromUrl(data.proxies);
+    });
+  }, [dispatch]);
 
   const onShowQrCode = (proxy: BaseProxy) => {
     setOperatingProxy(proxy);
     setIsQrcodeModalOpen(true);
-  }
+  };
 
   const columns: Array<TableColumnDefinition<BaseProxy>> = [
     createTableColumn<BaseProxy>({
@@ -102,7 +129,11 @@ export function Content(): React.ReactNode {
               }}
             >
               <DelayTag id={item.id} value={item.delay} />
-              <Operation proxy={item} onEdit={onEdit} onShowQrCode={onShowQrCode}/>
+              <Operation
+                proxy={item}
+                onEdit={onEdit}
+                onShowQrCode={onShowQrCode}
+              />
             </button>
           </TableCellLayout>
         );
@@ -133,13 +164,10 @@ export function Content(): React.ReactNode {
   const preProxiesLength = useRef(proxies.length);
   const listRef = useRef<HTMLDivElement>(null);
 
-  const [isEditingDialogOpen, setIsEditingDialogOpen] = useState(false);
   const [isQrcodeModalOpen, setIsQrcodeModalOpen] = useState(false);
 
-  const [operatingProxy, setOperatingProxy] = useState<BaseProxy | null>(null);
-
   const isOperatingProxySelected = useSelector<RootState, boolean>(
-      (state) => state.selected.proxy === operatingProxy?.id,
+    (state) => state.selected.proxy === operatingProxy?.id,
   );
 
   useEffect(() => {
@@ -154,41 +182,40 @@ export function Content(): React.ReactNode {
   }, [proxies.length]);
 
   return (
-      <>
-        {isQrcodeModalOpen && operatingProxy && (
-            <QrCodeModal
-                url={encode(operatingProxy as Shadowsocks)}
-                close={() => {
-                  setIsQrcodeModalOpen(false);
-                }}
+    <>
+      {isQrcodeModalOpen && operatingProxy && (
+        <QrCodeModal
+          url={encode(operatingProxy as Shadowsocks)}
+          close={() => {
+            setIsQrcodeModalOpen(false);
+          }}
+        />
+      )}
+      {isEditingDialogOpen && operatingProxy && (
+        <EditModal
+          close={() => {
+            setIsEditingDialogOpen(false);
+          }}
+          initialValue={operatingProxy}
+          type={operatingProxy.type as ProxyTypeEnum}
+          isSelected={isOperatingProxySelected}
+        />
+      )}
+      <div className={styles.wrapper} ref={listRef}>
+        {Object.keys(proxyMap).map((key) => {
+          return (
+            <ProxyCard
+              key={key}
+              columns={columns}
+              data={proxyMap[key]}
+              selectionMode={isAutoMode ? undefined : "single"}
+              onSelectionChange={handleSelect}
+              selectedItems={defaultSelectedItems}
+              url={key}
             />
-        )}
-        {isEditingDialogOpen && operatingProxy && (
-            <EditModal
-                close={() => {
-                  setIsEditingDialogOpen(false);
-                }}
-                initialValue={operatingProxy}
-                type={operatingProxy.type as ProxyTypeEnum}
-                isSelected={isOperatingProxySelected}
-            />
-        )}
-        <div className={styles.wrapper} ref={listRef}>
-          {Object.keys(proxyMap).map((key) => {
-            return (
-                <ProxyCard
-                    key={key}
-                    columns={columns}
-                    data={proxyMap[key]}
-                    selectionMode={isAutoMode ? undefined : "single"}
-                    onSelectionChange={handleSelect}
-                    selectedItems={defaultSelectedItems}
-                    url={key}
-                />
-            );
-          })}
-        </div>
-      </>
-
+          );
+        })}
+      </div>
+    </>
   );
 }
