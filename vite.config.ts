0c6e14e4ba811b80ba23712d837c0e49838bcb31
igoogolx <27353191+igoogolx@users.noreply.github.com>
2026-03-30 12:50:27 +0800

    chore: add vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "lux-js-sdk": path.resolve(__dirname, "modules", "lux-js-sdk"),
+    },
+  },
 });
