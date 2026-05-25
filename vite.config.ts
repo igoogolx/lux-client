6e1f05b8cba82a9d5d8395a2b2a439ef7f1f8cfa
igoogolx <27353191+igoogolx@users.noreply.github.com>
2026-05-26 00:12:34 +0800

    perf: enable react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import react from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    babel({
+      presets: [reactCompilerPreset()],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
