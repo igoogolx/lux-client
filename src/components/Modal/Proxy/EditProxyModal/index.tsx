9b1454cab0a83bbaf2cd24f89bedff40b594ac6a
igoogolx <27353191+igoogolx@users.noreply.github.com>
2026-08-22 21:45:17 +0800

    feat(ProxyModal): EditProxyModal

--- src/components/Modal/Proxy/EditProxyModal/index.tsx
+++ src/components/Modal/Proxy/EditProxyModal/index.tsx
@@ -1,6 +1,5 @@
 import { type ReactNode, useState } from "react";
 
-import { PasswordWidget } from "@/components/Core";
 import { TRANSLATION_KEY } from "@/i18n/locales/key.ts";
 import { proxiesSlice, type RootState } from "@/reducers";
 import { formatFormSchema } from "@/utils/form.ts";
@@ -9,106 +8,31 @@ import type { FormProps } from "@rjsf/core";
 import Form from "@rjsf/fluentui-rc";
 import type { RJSFSchema, UiSchema } from "@rjsf/utils";
 import validator from "@rjsf/validator-ajv8";
-import { addProxy, type Anytls, ProxyTypeEnum, updateProxy } from "lux-js-sdk";
+import { addProxy, type BaseProxy, updateProxy } from "lux-js-sdk";
 import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "./index.module.css";
 
-const schema: RJSFSchema = {
-  type: "object",
-  required: ["server", "port", "password"],
-  properties: {
-    name: {
-      type: "string",
-    },
-    server: {
-      type: "string",
-    },
-    port: {
-      type: "number",
-    },
-    password: {
-      type: "string",
-    },
-    sni: {
-      type: "string",
-    },
-    "client-fingerprint": {
-      type: "string",
-    },
-    fingerprint: {
-      type: "string",
-    },
-    certificate: {
-      type: "string",
-    },
-    "private-key": {
-      type: "string",
-    },
-    "idle-session-check-interval": {
-      type: "number",
-    },
-    "idle-session-timeout": {
-      type: "number",
-    },
-    "min-idle-session": {
-      type: "number",
-    },
-    "skip-cert-verify": {
-      type: "boolean",
-    },
-
-    alpn: {
-      type: "array",
-      items: {
-        type: "string",
-      },
-    },
-  },
-};
-
-const uiSchema: UiSchema = {
-  password: {
-    "ui:widget": PasswordWidget,
-    "ui:autocomplete": "off",
-  },
-};
-
-type EditAnytlsModalProps = {
+type EditProxyModalProps<T> = {
   onClose: () => void;
-  initialValue?: Anytls;
+  initialValue: T;
   isSelected?: boolean;
+  schema: RJSFSchema;
+  uiSchema: UiSchema;
+  renderFieldTitle: (filedKey: string) => string | null;
 };
 
-const INIT_DATA: Anytls = {
-  type: ProxyTypeEnum.Anytls,
-  id: "",
-  name: "",
-  server: "",
-  password: "",
-  port: 1080,
-};
-
-const FIELD_TITLE_I18N_KEY: Record<string, string> = {
-  name: TRANSLATION_KEY.FORM_NAME,
-  server: TRANSLATION_KEY.FORM_SERVER,
-  password: TRANSLATION_KEY.FORM_PASSWORD,
-  port: TRANSLATION_KEY.FORM_PORT,
-  sni: TRANSLATION_KEY.FORM_SNI,
-  "client-fingerprint": TRANSLATION_KEY.FORM_CLIENT_FINGERPRINT,
-  fingerprint: TRANSLATION_KEY.FORM_FINGERPRINT,
-  certificate: TRANSLATION_KEY.FORM_CERTIFICATE,
-  "private-key": TRANSLATION_KEY.FORM_PRIVATE_KEY,
-  "idle-session-check-interval":
-    TRANSLATION_KEY.FORM_IDLE_SESSION_CHECK_INTERVAL,
-  "idle-session-timeout": TRANSLATION_KEY.FORM_IDLE_SESSION_TIMEOUT,
-  "min-idle-session": TRANSLATION_KEY.FORM_MIN_IDLE_SESSION,
-  "skip-cert-verify": TRANSLATION_KEY.SKIP_CERT_VERIFY,
-  alpn: TRANSLATION_KEY.FORM_ALPN,
-};
-
-export function EditProxyModal(props: EditAnytlsModalProps): ReactNode {
-  const { initialValue, isSelected, onClose } = props;
+export function EditProxyModal<T extends BaseProxy>(
+  props: EditProxyModalProps<T>,
+): ReactNode {
+  const {
+    initialValue,
+    isSelected,
+    onClose,
+    schema,
+    uiSchema,
+    renderFieldTitle,
+  } = props;
 
   const dispatch = useDispatch();
   const { t } = useTranslation();
@@ -117,16 +41,9 @@ export function EditProxyModal(props: EditAnytlsModalProps): ReactNode {
     (state) => state.manager.isStared,
   );
 
-  const [formData, setFormData] = useState<Anytls>(initialValue || INIT_DATA);
+  const [formData, setFormData] = useState<T>(initialValue);
 
-  const transFieldTitle = (key: string) => {
-    if (key in FIELD_TITLE_I18N_KEY) {
-      return t(FIELD_TITLE_I18N_KEY[key] as string);
-    }
-    return null;
-  };
-
-  const renderedSchema = formatFormSchema(schema, transFieldTitle);
+  const renderedSchema = formatFormSchema(schema, renderFieldTitle);
 
   const handleChange: FormProps["onChange"] = (e) => {
     setFormData(e.formData);
