c1920e2390f0fc05e1fe2fc5d00ca03707916c05
igoogolx <27353191+igoogolx@users.noreply.github.com>
2026-09-14 18:01:01 +0800

    chore: remove vite-plugin-checker

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import babel from "@rolldown/plugin-babel";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
-import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,9 +9,6 @@ export default defineConfig({
     react(),
     babel({
       presets: [reactCompilerPreset()],
-    }),
-    checker({
-      typescript: true,
     }),
   ],
   resolve: {
